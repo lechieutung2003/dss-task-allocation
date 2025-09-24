9b06b64ee567e82612ae686d2bef37a1e2d2eea9
cestunchat <mailehongphuc52003@gmail.com>
2025-09-24 21:00:56 +0700

    feat: add orders pages (list, create, details)

---
+++ business/pages/dss/orders/[id].vue
@@ -0,0 +1,6 @@
+<template>
+  <div>Chi tiết đơn + phân công (admin)</div>
+</template>
+<script setup>
+// ...
+</script>
---
+++ business/pages/dss/orders/create.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>Tạo đơn mới (client)</div>
+</template>
+<script setup>
+// ...
+</script>
