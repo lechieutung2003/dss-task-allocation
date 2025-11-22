6ed939ff7d49b94cac7d81ac262d20b92d8cafc4
vothithao <0945206559vtt@gmail.com>
2025-11-22 18:15:56 +0700

    update: update localtime

--- backend/hr/serializers/order.py
+++ backend/hr/serializers/order.py
@@ -4,6 +4,7 @@ from ..models.customer import Customer, ServiceType
 from businesses.serializers.employee import EmployeeShortSerializer
 from businesses.serializers.employee import EmployeeShortSerializer
 from .customer import CustomerSerializer, ServiceTypeSerializer
+from django.utils.timezone import localtime
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,6 +15,8 @@ class ServiceTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceType
         fields = '__all__'
+
+
 
 class OrderSerializer(serializers.ModelSerializer):
     customer_details = CustomerSerializer(source='customer', read_only=True)
@@ -27,17 +30,18 @@ class OrderSerializer(serializers.ModelSerializer):
         try:
             representation = super().to_representation(instance)
             representation['customer_name'] = f"{instance.customer.name}" if instance.customer else ""
+            # Chuyển đổi thời gian sang giờ địa phương
+            representation['created_at_local'] = localtime(instance.created_at).strftime('%Y-%m-%d %H:%M:%S')
+            representation['updated_at_local'] = localtime(instance.updated_at).strftime('%Y-%m-%d %H:%M:%S')
             return representation
         except ValueError as e:
             if "badly formed hexadecimal UUID string" in str(e):
                 print(f"Lỗi UUID với order ID: {instance.id}")
-                # Trả về một representation tạm thời
                 return {
                     'id': str(instance.id),
                     'error': 'Dữ liệu không hợp lệ'
                 }
             raise e
-
 class OrderEmployeeSerializer(serializers.ModelSerializer):
     """Serializer cho employee chỉ có thể edit status"""
     
